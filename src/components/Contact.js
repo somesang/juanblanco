b5c530e2d35865842a703a43ebcc8ac843cdcb45
somesang <sanyoeun@gmail.com>
2018-12-10 17:06:25 -0500

    add source link

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -8,6 +8,7 @@ class Contact extends Component {
 				<p>Don't hesitate to email/call/text me anytime for help, questions or whatever.</p>
 				<p>616 546 5436</p>
 				<p>sang @ fairlypainless.com</p>
+				<p>Here's the source code for this site: <a href="https://github.com/somesang/juanblanco" target="_blank">https://github.com/somesang/juanblanco</a></p>
 			</div>
 		)
 	}
